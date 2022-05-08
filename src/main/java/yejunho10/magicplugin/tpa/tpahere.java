7498cfd2fa0f3ae6a80c909180e2548f76ed9c1d
yejunho10 <98728918+yejunho10@users.noreply.github.com>
2022-05-08 20:04:08 +0900

    tpa 클래스 생성

--- src/main/java/yejunho10/magicplugin/tpa/tpahere.java
+++ src/main/java/yejunho10/magicplugin/tpa/tpahere.java
@@ -4,6 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+@SuppressWarnings("all")
 public class tpahere implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
--- src/main/java/yejunho10/magicplugin/tpa/tpask.java
+++ src/main/java/yejunho10/magicplugin/tpa/tpask.java
@@ -4,9 +4,23 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+@SuppressWarnings("all")
 public class tpask implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         return false;
     }
 }
+
+/*
+new BukkitRunnable() {
+    @Override
+        public void run() {
+            코드 내용!
+        }
+}.runTaskLaterAsynchronously(GUIPlugin.getInstance(), 20 * 15);
+
+
+-> 15초후에 실행한다. <-
+-> tpa 신청 내역 삭제에 이용된다 <-
+*/
